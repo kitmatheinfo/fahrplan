0000000819f3631175a68060e6d4798c29b52c59
Janina Wibker <me@janina.lol>
2023-09-17 19:52:26 +0200

    Customize title, favicon and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ const inter = Inter({
 })
 
 export const metadata = {
-  title: 'Fahrplan',
+  title: 'Fahrplan | kitmatheinfo.de',
+  description: 'O-Phasen Termine: Das Wann und Wo',
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗓️</text></svg>'
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦆</text></svg>'
   }
 }
 
